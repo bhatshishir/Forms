e7a73225cf99678a63c4628e4e1f53bb3744efdc
shishir <shishirbhat2000@gmail.com>
2023-04-24 11:10:02 +0530

    simple changes

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -1,11 +1,38 @@
-import React from 'react'
+import React from "react";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+import { useRef } from "react";
+
+const schema = yup
+  .object()
+  .shape({
+    name: yup.string().required(),
+    age: yup.string().required("age is required").matches(/[0-9]/,"should be digit"),
+  })
 
 const Form = () => {
-  return (
-    <div>
-        
-    </div>
-  )
-}
+    const ref=useRef();
+  const { register, handleSubmit, formState } = useForm({
+    resolver: yupResolver(schema),
+    mode:"onTouched"
+  });
+  const {errors}=formState;
+  console.log(errors);
 
-export default Form
+  return (
+    <form onSubmit={handleSubmit((d) => console.log(d))}>
+      <label htmlFor="name">Name</label>
+      <input
+        name="name"
+        {...register("name")}
+      />
+      <p>{errors.name?.message}</p>
+      <label htmlFor="age">Age</label>
+      <input name="age" {...register("age")} />
+      <p>{errors.age?.message}</p>
+      <input type="submit" />
+    </form>
+  );
+};
+export default Form;
